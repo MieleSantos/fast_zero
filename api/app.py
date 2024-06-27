feea0baf906985e52db0c452b551ca66f22a2b8d
mielesantos <mielesnts@gmail.com>
2024-06-26 21:35:17 -0300

    add crud

--- api/app.py
+++ api/app.py
@@ -1,14 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 
-from .schemas import Message, UserPublic, UserSchema, userDB
+from .schemas import Message, UserList, UserPublic, UserSchema, userDB
 
 app = FastAPI()
 
 # mock database
-databases: list = []
+database: list = []
 
 
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
@@ -16,11 +16,36 @@ def read_root():
     return {"message": "Olá Mundo!"}
 
 
+@app.get("/users/", response_model=UserList)
+def read_users():
+    return {"users": database}
+
+
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
-    user_with_id = userDB(id=len(databases) + 1, **user.model_dump())
-    databases.append(user_with_id)
+    user_with_id = userDB(id=len(database) + 1, **user.model_dump())
+    database.append(user_with_id)
     return user_with_id
+
+
+@app.put("/users/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+    user_with_id = userDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete("/users/{user_id}", response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User not found")
+
+    del database[user_id - 1]
+
+    return {"message": "User deleted"}
 
 
 @app.get("/html", status_code=200, response_class=HTMLResponse)
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,36 +12,67 @@ def client():
 
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
-
     response = client.get("/")  # Act
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {"message": "Olá Mundo!"}  # Assert
 
 
-def test_create_user(client):
+def test_root_deve_retornar_ok_e_ola_mundo_html(client):
+    response = client.get("/html")  # Act
 
+    assert response.status_code == HTTPStatus.OK  # Assert
+
+
+def test_create_user(client):
     response = client.post(  # UserShema
         "/users/",
         json={
-            "username": "teste1",
+            "username": "string",
             "password": "senha",
-            "email": "test@teste.com",
+            "email": "user@example.com",
         },
     )
     # Voltou 201?
     assert response.status_code == HTTPStatus.CREATED
 
-    # validar UserPublic,
+    # validar UserPublic
     assert response.json() == {
-        "username": "teste1",
         "id": 1,
-        "email": "test@teste.com",
+        "username": "string",
+        "email": "user@example.com",
     }
 
 
-def test_root_deve_retornar_ok_e_ola_mundo_html(client):
+def test_read_users(client):
+    response = client.get("/users/")
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {"id": 1, "username": "string", "email": "user@example.com"},
+        ]
+    }
 
-    response = client.get("/html")  # Act
 
-    assert response.status_code == HTTPStatus.OK  # Assert
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "username": "string",
+            "email": "user@example.com",
+            "password": "senha",
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "id": 1,
+        "username": "string",
+        "email": "user@example.com",
+    }
+
+
+def test_delete_user(client):
+    response = client.delete("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {"message": "User deleted"}
